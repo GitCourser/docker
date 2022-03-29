e171b5a9ac59baea4a45c0a9dca55acaa562082e
Courser <GitCourser@users.noreply.github.com>
2022-03-29 17:10:03 +0800

    Update build.sh

--- arm64/build.sh
+++ arm64/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y --no-install-recommends qemu-user-static binfmt-support
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends qemu-user-static binfmt-support
 ls /usr/bin/qemu-*
 update-binfmts --enable qemu-aarch64
