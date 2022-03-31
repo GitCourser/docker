28a039fab0d14fd4aaa7747a038211f76da1dfd5
Courser <GitCourser@users.noreply.github.com>
2022-03-31 16:35:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ DOCKER_BUILDKIT=1 docker build \
     --build-arg BUILDKIT_INLINE_CACHE=1 \
     --build-arg ARG_APT_NO_RECOMMENDS=1 \
     -t "$user"/ubuntu:"$platform" .
+
 docker images
 
 docker login -u "$user" -p "$passwd"
