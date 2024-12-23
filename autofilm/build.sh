f1036870c7ed92e24ccc8087023b1e846b63e1e7
Courser <GitCourser@users.noreply.github.com>
2024-12-23 11:46:43 +0800

    Add files via upload

---
+++ autofilm/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+repo="Akimio521/AutoFilm"
+latest=$(curl -s https://api.github.com/repos/$repo/releases/latest)
+app_ver=$(echo $latest | jq -r '.tag_name')
+tag_ver=${version#v}
+
+image_name="$user/autofilm"
+tags=$(curl -s "https://hub.docker.com/v2/repositories/$image_name/tags/" | jq -r '.results[].name')
+if echo $tags | grep -q $tag_ver; then
+  echo "$tag_ver 已存在"
+  exit 0
+fi
+
+echo "开始构建 $tag_ver ..."
