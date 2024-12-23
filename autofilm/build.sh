4fa9d30a5e391b3eef0a54b326753393ff4b8960
Courser <GitCourser@users.noreply.github.com>
2024-12-23 13:17:44 +0800

    Update build.sh

--- autofilm/build.sh
+++ autofilm/build.sh
@@ -2,19 +2,12 @@
 
 repo="Akimio521/AutoFilm"
 latest=$(curl -s https://api.github.com/repos/$repo/releases/latest)
-latest=$(echo "$latest" | tr -d '\r\n')
-echo $latest
+latest=$(echo "$latest" | tr -d '\r')
 app_ver=$(echo $latest | jq -r '.tag_name')
-echo $app_ver
 tag_ver=${app_ver#v}
-echo $tag_ver
-echo "${app_ver#v}"
 
-echo "$user"
 image_name="$user/autofilm"
-echo $image_name
 tags=$(curl -s "https://hub.docker.com/v2/repositories/$image_name/tags/" | jq -r '.results[].name')
-echo $tags
 if echo $tags | grep -q $tag_ver; then
   echo "$tag_ver 已存在"
   exit 0
