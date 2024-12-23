ebc4a39ec588e88d570463852c707255bf1b95cd
Courser <GitCourser@users.noreply.github.com>
2024-12-23 11:51:24 +0800

    Update build.sh

--- autofilm/build.sh
+++ autofilm/build.sh
@@ -6,7 +6,9 @@ app_ver=$(echo $latest | jq -r '.tag_name')
 tag_ver=${version#v}
 
 image_name="$user/autofilm"
+echo $image_name
 tags=$(curl -s "https://hub.docker.com/v2/repositories/$image_name/tags/" | jq -r '.results[].name')
+echo $tags
 if echo $tags | grep -q $tag_ver; then
   echo "$tag_ver 已存在"
   exit 0
